0bfa6865cc58a7f8348cf492205b349b56a8af8c
vimcy7 <vimcy7@gmail.com>
2025-03-11 17:11:37 +0530

    Setup devcontainer for F* development

---
+++ fstar/lib/fstar/lib/FStarC_Version.ml
@@ -0,0 +1,6 @@
+let dummy () = ();;
+FStarC_Options._version := "2024.12.03";
+FStarC_Options._platform := "Linux_x86_64";;
+FStarC_Options._compiler := "OCaml 4.14.2";;
+FStarC_Options._date := "2024-12-04 20:59:27 -0800";;
+FStarC_Options._commit:= "b0961063393215ca65927f017720cb365a193833-dirty";;
